3bd160cf2414b4149111e9238147914a5b4d3416
Advithiwhitehat <74882349+Advithiwhitehat@users.noreply.github.com>
2021-09-01 16:44:51 -0700

    Add files via upload

---
+++ screens/CreatePost.js
@@ -0,0 +1,129 @@
+import { View, Text, Image, ScrollView, SafeAreaView, TextInput, StyleSheet} from "react-native";
+import React from "react";
+import { RFValue } from "react-native-responsive-fontsize";
+import Ionicons from 'react-native-vector-icons/Ionicons';
+import DropDownPicker from "react-native-dropdown-picker";
+import AppLoading from "expo-app-loading";
+import * as Font from "expo-font";
+import { Caption } from "react-native-paper";
+
+let customFonts = {
+  "Bubblegum-Sans": require("../assets/fonts/BubblegumSans-Regular.ttf")
+};
+export default class CreatePost extends Component{
+    constructor(props){
+        super();
+        this.state={
+            previewImage: "image_1.png",
+            fontsLoaded: false,
+            dropdownHeight: 40
+            
+        }
+    }
+
+    async _loadFontsAsync() {
+        await Font.loadAsync(customFonts);
+        this.setState({ fontsLoaded: true });
+      }
+    
+      componentDidMount() {
+        this._loadFontsAsync();
+      }
+    render(){
+        if (!this.state.fontsLoaded) {
+            return <AppLoading />;
+          } else {
+            let preview_images = {
+              image_1: require("../assets/image_1.png"),
+              image_2: require("../assets/image_2.png"),
+              image_3: require("../assets/image_3.png"),
+              image_4: require("../assets/image_4.png"),
+              image_5: require("../assets/image_5.png")
+            };
+        return(
+            <View style={styles.container}>
+                <View style={styles.droidSafeArea}>
+                    <View style={styles.appTitle}>
+                        <View style={styles.appIcon}>
+                        <Image source ={require("../assets/logo.png")}
+                               style={styles.iconImage}
+                               ></Image>
+                        </View>
+                        <View style={styles.appTitleTextcontainer}>
+                            <Text style={styles.appTitleText}>New Post</Text>
+                        </View>
+                    </View>
+                    <View style={styles.fieldsContainer}>
+                        <ScrollView>
+                            <Image source={preview_images[this.state.previewImage]} 
+                           style={styles.previewImage}
+                           ></Image>
+
+                    <View style={{height:RFValue(this.state.dropdownHeight)}}>
+                    <DropDownPicker
+                        items={[
+                            {label:"Image 1 ", value:"image_1"},
+                            {label:"Image 2 ", value:"image_2"},
+                            {label:"Image 3 ", value:"image_3"},
+                            {label:"Image 4 ", value:"image_4"},
+                            {label:"Image 5 ", value:"image_5"},
+                            {label:"Image 6 ", value:"image_6"},
+                            {label:"Image 7 ", value:"image_7"}
+                        ]}
+                        defaultValue={this.state.previewImage}
+                        containerStyle={{
+                            height:40,
+                            borderRadius: 20,
+                            marginBottom:10
+                        }}
+                        onOpen={ ()=>{
+                            this.setState({dropdownHeight:170})
+                        }}
+                        onClose={()=>
+                        this.setState({dropdownHeight:40})}
+                        style={{backgroundColor:"transparent"}}
+                        itemStyle={{
+                            justifyContent:"flex-start"
+                        }}
+                        dropDownStyle={{backgroundColor:"#2a2a2a"}}
+                        labelStyle={{
+                            color:"white"
+                        }}
+                        arrowStyle={{
+                            color: "white"
+                        }}
+                        onChangeItem={item=>
+                        this.setState({preview_images:item.value})}
+                    />
+                </View>
+                <TextInput
+                    style={styles.inputFont}
+                    onChangeText={caption=> this.setState({caption})}
+                    placeholder={"Caption"}
+                    placeholderTextColor="white"
+                    />
+                </ScrollView>
+            </View>
+            <View style={{fles:0.08}}/>
+            </View>
+            </View>
+        );
+    }
+}
+}
+
+const styles = StyleSheet.create({
+    container={
+        flex:0.85,
+        justifyContent:"center"
+    },
+    image={
+        width:"100%",
+        height:"100%",
+        resizeMode:"contain"
+    },
+    text={
+        color:"black",
+        fontSize:RFValue(28)
+    }
+})
